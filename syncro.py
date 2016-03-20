8f900453e06d9093228f38b9c348ee1c0ae3a173
nithishdivakar <nd@gmail.com>
2016-03-20 16:38:51 +0530

    adding and deleting all files in a folder

--- syncro.py
+++ syncro.py
@@ -18,19 +18,21 @@ def sync_out():
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-a", "--add-file", help="add a new file to keep track of")
-parser.add_argument("-r","--remove-file", help="remove a file from keeping track of")
+parser.add_argument("-a", "--add-file", nargs='+',  help="add a new file to keep track of")
+parser.add_argument("-r","--remove-file",nargs='+', help="remove a file from keeping track of")
 parser.add_argument("-ls", "--list", help="list all files", action='store_true')
 parser.add_argument("--sync-in", help="backup every file", action='store_true')
 parser.add_argument("--sync-out", help="restore every file", action='store_true')
 args = parser.parse_args()
 
 if args.add_file:
-    composer.add_file(args.add_file)
+    for F in args.add_file:
+        composer.add_file(F)
     print "done !!"
 
-if args.remove_file:
-    composer.remove_file(args.remove_file)
+if args.remove_file: 
+    for F in args.remove_file:
+        composer.remove_file(F)
     print "done !!"
 
 if args.list: composer.list_files()
